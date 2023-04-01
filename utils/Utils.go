a30d04fd00f932003caf0e94306e6c61fa08ee26
Hynek Uhlíř <hynek.uhlir@gmail.com>
2023-04-01 10:09:11 +0200

    Ampersand in path fix

--- utils/Utils.go
+++ utils/Utils.go
@@ -2,18 +2,14 @@ package utils
 
 import (
 	"net/url"
+	"strings"
 )
 
 func EncodeURL(value string) string {
 	// https://go.dev/play/p/pOfrn-Wsq5
-	url := &url.URL{Path: value}
-	// URL předsadí před sebe './'
-	encoded := url.String()
-	if len(encoded) >= 2 && encoded[:2] == "./" {
-		encoded = encoded[2:]
-	}
+	encoded := url.QueryEscape(value)
 	// VLC má vadu a nebere URL mezery jako '+', zvládá jen '%20'
-	//encoded := url.QueryEscape("file:///D:/Hudba/" + value)
+	encoded = strings.ReplaceAll(encoded, "+", "%20")
 	return encoded
 }
 
